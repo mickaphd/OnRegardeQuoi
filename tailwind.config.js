1f2b1a4ad26bcb3724188d0cf9cff249ffd95f8b
mickaphd <mickaphd@users.noreply.github.com>
2026-01-22 16:15:22 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        amber: { 400: '#FBBF24' },
+        gray: { 400: '#9CA3AF' },
+        background: '#080808',
+      },
+      fontFamily: {
+        slackey: ['Slackey', 'cursive'],
+      }
+    }
+  }
+}
